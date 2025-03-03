4045b242b2f3ad4465d89a21e1c59ad9cd929ef4
JKFullDev <jcah009@gmail.com>
2025-03-03 11:54:58 +0100

    1 y 2

---
+++ ejercicio1.js
@@ -0,0 +1,36 @@
+const movies = [
+    {
+        title: "Bracula: Condemor II",
+        duration: 192,
+        categories: ["comedia", "aventura"],
+    },
+    {
+        title: "Spider-Man: No Way Home",
+        duration: 122,
+        categories: ["aventura", "acción"],
+    },
+    {
+        title: "The Voices",
+        duration: 223,
+        categories: ["comedia", "thriller"],
+    },
+    {
+        title: "Shrek",
+        duration: 111,
+        categories: ["comedia", "aventura", "animación"],
+    },
+];
+
+
+const categories = [];
+
+for (const movie of movies) {
+    for (const category of movie.categories) {
+        // Si la categoría no está en el array categories, la añadimos
+        if (!categories.includes(category)) {
+            categories.push(category);
+        }
+    }
+}
+
+console.log(categories);
---
+++ ejercicio2.js
@@ -0,0 +1,48 @@
+const users = [
+    {
+        name: "Alberto",
+        favoritesSounds: {
+            waves: { format: "mp3", volume: 50 },
+            rain: { format: "ogg", volume: 60 },
+            firecamp: { format: "mp3", volume: 80 },
+        },
+    },
+    {
+        name: "Antonio",
+        favoritesSounds: {
+            waves: { format: "mp3", volume: 30 },
+            shower: { format: "ogg", volume: 55 },
+            train: { format: "mp3", volume: 60 },
+        },
+    },
+    {
+        name: "Santiago",
+        favoritesSounds: {
+            shower: { format: "mp3", volume: 50 },
+            train: { format: "ogg", volume: 60 },
+            firecamp: { format: "mp3", volume: 80 },
+        },
+    },
+    {
+        name: "Laura",
+        favoritesSounds: {
+            waves: { format: "mp3", volume: 67 },
+            wind: { format: "ogg", volume: 35 },
+            firecamp: { format: "mp3", volume: 60 },
+        },
+    },
+];
+
+let volumeSum = 0;
+let volumeCount = 0;
+
+for (const user of users) {
+    for (const sound in user.favoritesSounds) {
+        volumeSum += user.favoritesSounds[sound].volume;
+        volumeCount++;
+    }
+}
+
+avgVolume = volumeSum / volumeCount;
+
+console.log(`La media del volumen de todos los sonidos favoritos es: ${avgVolume}`);
